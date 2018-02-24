e800da84e44fb32c53533fd2ebb30048b5ddd7bc
Tim Sharp <timothyaaron.sharp@gmail.com>
2018-02-24 14:49:02 -0700

    moved main files to top level

--- preload.js
+++ preload.js
@@ -8,12 +8,12 @@ Preload.prototype = {
     preload: function () {
 
         //first argument is the "key", used to refer to when creating in the "Create state. Second argument is the path to the asset to preload.
-        this.game.load.image('sky', '../assets/sky.png')
-        this.game.load.image('star', '../assets/star.png')
-        this.game.load.image('diamond', '../assets/diamond.png')
-        this.game.load.image('ground', '../assets/platform.png');
+        this.game.load.image('sky', '/assets/sky.png')
+        this.game.load.image('star', '/assets/star.png')
+        this.game.load.image('diamond', '/assets/diamond.png')
+        this.game.load.image('ground', '/assets/platform.png');
 
-        this.game.load.spritesheet('dude', '../assets/dude.png', 32, 48)
+        this.game.load.spritesheet('dude', '/assets/dude.png', 32, 48)
 
         //adding keys to the payload for the "Create" state to reference when creating assets
         // this.payload.sky = 'sky'
